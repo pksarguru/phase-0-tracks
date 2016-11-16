64b26eced966011f2916dff0bb944ce67b9ce367
Pavan Sarguru <pavansarguru@gmail.com>
2016-11-15 21:41:20 -0600

    Finish Release 1

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,8 +2,12 @@ puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
 age = gets.chomp
-puts "Cool. What year were you born?"
+puts "Cool. What year were you born? (19xx)"
 year = gets.chomp
+while 
+  puts "Would you like any garlic bread?  Please answer using 'y' or 'n'."
+  bread=gets.chomp
+end 
 puts "Would you like any garlic bread (y/n)?"
 bread = gets.chomp
 while bread != "y" && bread != "n"
@@ -16,3 +20,21 @@ while insurance != "y" && insurance != "n"
   puts "Would you like to enroll in the company health insurance?  Please enter 'y' or 'n'."
   insurance=gets.chomp
 end 
+
+
+# Check if the vampire(?) can add...
+
+vampire_age = false
+
+if 2016 - year.to_i == age.to_i
+  vampire_age = true
+end
+
+
+if bread == "y"
+  vampire_bread = true
+else 
+  vampire_bread = false
+end
+
+puts vampire_bread
