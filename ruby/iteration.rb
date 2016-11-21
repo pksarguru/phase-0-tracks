e401b348bc6f1549a0d9c6302630b35174ebee0b
Erica42 <erica.forget@gmail.com>
2016-11-21 16:57:21 -0600

    release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,7 +6,7 @@ def block_method
 end
 block_method { |name1, name2| puts "So good to see you #{name2} and #{name1}!" }
 
-yarn = [ 'acrylic', 'wool', 'bamboo', 'silk']
+yarn = [ 'acrylic', 'wool', 'bamboo', 'silk' ]
 
 
 yarn_hash = {
@@ -28,3 +28,60 @@ end
 
 p yarn
 p yarn_hash
+
+numbers_again = [ 1, 2, 25, 42, 57]
+
+numbers_again.delete_if { |num| num < 5 }
+#delete_if is danger danger will robinson!
+
+letters_hash = {
+  :a => 1,
+  :b => 2,
+  :c => 3,
+  :d => 4
+}
+
+letters_hash.delete_if { |letter, number| letter >= :b }
+#also dangerous!
+
+p letters_hash
+
+
+p numbers_again
+
+numbers_again = [ 1, 2, 25, 42, 57]
+
+numbers_again.keep_if { |num| num < 5 }
+#keep_if is also danger!
+p numbers_again
+
+letters_hash = {
+  :a => 1,
+  :b => 2,
+  :c => 3,
+  :d => 4
+}
+
+letters_hash.keep_if { |letter, number| letter >= :b}
+#destructive keep
+p letters_hash
+
+numbers_again = [ 1, 2, 25, 42, 57]
+
+numbers_again.drop_while { |num| num < 5 }
+
+p numbers_again
+
+letters_hash = {
+  :a => 1,
+  :b => 2,
+  :c => 3,
+  :d => 4
+}
+
+letters_hash.has_key?(:b)
+#not destructive
+p letters_hash
+
+
+
