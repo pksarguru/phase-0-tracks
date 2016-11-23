01d3fd00fd170d89f6155cdc9699463f4b0f963b
Erica42 <erica.forget@gmail.com>
2016-11-22 18:42:22 -0600

    Finished

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,6 +1,6 @@
 
-#business code
-h = {
+#business code / create hash
+preferences = {
   name: "a",
   age: 0,
   number_of_kids: 0,
@@ -12,49 +12,43 @@ h = {
 #Driver
 #Prompt user for info and convert if needed
 puts "Please enter the name of the client:"
-h[:name] = gets.chomp
+preferences[:name] = gets.chomp
 
 puts "Please enter age of client:"
-h[:age] = gets.chomp.to_i
+preferences[:age] = gets.chomp.to_i
 
 puts "Please enter number of children:"
-h[:number_of_kids] = gets.chomp.to_i
+preferences[:number_of_kids] = gets.chomp.to_i
 
 puts "Please enter the theme of the decor:"
-h[:decor_theme] = gets.chomp
+preferences[:decor_theme] = gets.chomp
 
 puts "Are we on a budget?"
 money = gets.chomp.downcase
 if money == "yes"
-  h[:wealthy] = false
+  preferences[:wealthy] = false
 elsif money == "no"
-  h[:wealthy] = true
+  preferences[:wealthy] = true
 else
-  h[:wealthy] = nil
+  preferences[:wealthy] = nil
 end
 
 #print the hash
-p h
+p preferences
 
 #update a key
 puts "If you need to update a key, type in the name of the key, or type none to skip."
 old_key = gets.chomp.downcase
 
-def change_key(x)
-  p "What would you like the new key name to be?"
-  new_key = gets.chomp.to_sym
-  h[new_key] = h.delete(x)
-end
-
 if old_key != "none"
   old_key = old_key.to_sym
-  change_key(old_key)
-  p "not none"
+  #ask for new key name
+  p "What would you like the new key name to be?"
+  new_key = gets.chomp.to_sym
+  preferences[new_key] = preferences.delete(old_key)
 else
   p "All done."
 end
 
-
-
 #print the hash
-p h
+p preferences
