0d0a851e09fe7176f87061d63206a0277b92b21b
Erica42 <erica.forget@gmail.com>
2016-11-25 17:37:20 -0600

    All done

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -36,6 +36,18 @@ end
 #debugging
 #p next_consonant("b")
 
+# Original flip code that I then added to.
+# def flip_name(name)
+#   new_name = name.downcase.split(" ")
+#   #capitalize first and last name
+#   new_name[0] = new_name[0].capitalize!
+#   new_name[1] = new_name[1].capitalize!
+#   #rejoin
+#   new_name = new_name[1] + ' ' + new_name[0]
+#   #debug
+#   p new_name
+# end
+
 def flip_name(name)
   new_name = name.downcase.split(" ")
 #debugging new_name = ["forget", "erica"]
@@ -84,98 +96,15 @@ name_hash = {}
 while original_name != "quit"
   flip_name(original_name)
   name_hash.store(original_name, flip_name(original_name))
-  puts name_hash
+  #debugging
+  #puts name_hash
   puts "Any other name you'd like to try?"
   original_name = gets.chomp
 end
 name_hash.each {|key, value| puts "#{key} is also known as #{value}."}
 
-# h = { "a" => 100, "b" => 200 }
-# h.each {|key, value| puts "#{key} is #{value}" }
 
 
 
 
 
-
-
-
-
-
-
-
-# def next_vowel(v)
-#   str = "aeiou"
-#   num = str.index(v)
-#   if num != 4
-#     num += 1
-#   else
-#     num = 0
-#   end
-#   new_v = str[num]
-#   #debug
-#   p new_v
-# end
-
-# def next_consonant(c)
-#   str = "bcdfghjklmnpqrstvwxyz"
-#   num = str.index(c)
-#   if num != 20
-#     num += 1
-#   else
-#     num = 0
-#   end
-#   new_c = str[num]
-#   #debug
-#   p new_c
-# end
-
-# def flip_name(name)
-#   new_name = name.downcase.split(" ")
-#   #capitalize first and last name
-#   new_name[0] = new_name[0].capitalize!
-#   new_name[1] = new_name[1].capitalize!
-#   #rejoin
-#   new_name = new_name[1] + ' ' + new_name[0]
-#   #debug
-#   p new_name
-# end
-
-# def next_vowel(v)
-#   str = "aeiou"
-#   num = str.index(v)
-#   if num != 4
-#     num += 1
-#   else
-#     num = 0
-#   end
-#   new_v = str[num]
-# end
-# p next_vowel("a")
-
-# def next_consonant(c)
-#   str = "bcdfghjklmnpqrstvwxyz"
-#   num = str.index(c)
-#   if num != 20
-#     num += 1
-#   else
-#     num = 0
-#   end
-#   new_c = str[num]
-# end
-# p next_consonant("b")
-
-# new_name = ["forget", "erica"]
-# first_name = ""
-# i = 0
-# while i < new_name[0].length
-#     if (new_name[0][i]=="a" || new_name[0][i]=="e" || new_name[0][i]=="i" || new_name[0][i]=="o"|| new_name[0][i]=="u")
-#       first_name = first_name + next_vowel(new_name[0][i])
-#       i += 1
-#     else
-#       first_name = first_name + next_consonant(new_name[0][i])
-#       i += 1
-#     end
-
-# end
-# p first_name
