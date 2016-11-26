50e5878c7bc1917d0fa65459a1e5e8944e3cd4df
Pavan Sarguru <pavansarguru@gmail.com>
2016-11-26 00:27:07 -0600

    Edit loops

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,26 +1,23 @@
-arr = [34, 68, 4, 5, 68]
+arr = [68, 68, 4, 5, 68]
 
 def search_array(array, number)
-    index = 0
-    until index > array.length
+    n = array.length
+    (n-1).times do |index|
       if number == array[index]
         puts index
       end
-      index += 1
     end
-  end
+end
 
 puts search_array(arr, 68)
 
 def fib(index)
   fib_array = [0,1]
-  i = 2
-  while i<index
-    fib_array << (fib_array[i-1]+fib_array[i-2])
-    i += 1
+  (index-2).times do |i|
+    fib_array << (fib_array[i]+fib_array[i+1])
   end
   return fib_array
 end
 
-p fib(6)
+p fib(10)
 
