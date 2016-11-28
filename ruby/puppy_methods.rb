643ecd50cc0ab2d66c32dace2fac9812583e29ac
Erica42 <erica.forget@gmail.com>
2016-11-28 14:39:18 -0600

    Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,33 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    num.times { puts "Woof!" }
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_age)
+    human_age * 7
+  end
+
+  def sic(name)
+    puts "#{name} is being horribly attacked by puppy licks!"
+  end
+
+
 end
 
+
+
+# Driver Code
+fido = Puppy.new
+toy = "rope bone"
+fido.fetch(toy)
+
+fido.sic("Ollie")
+p fido.dog_years(11)
+fido.roll_over
+fido.speak(3)
