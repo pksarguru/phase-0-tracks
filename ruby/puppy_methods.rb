865042244f8f81872acc3505de7815e11c915d9a
Erica42 <erica.forget@gmail.com>
2016-11-28 14:46:26 -0600

    Added initialize to Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,36 +1,29 @@
 class Puppy
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
   def speak(num)
     num.times { puts "Woof!" }
   end
-
   def roll_over
     puts "*rolls over*"
   end
-
   def dog_years(human_age)
     human_age * 7
   end
-
   def sic(name)
     puts "#{name} is being horribly attacked by puppy licks!"
   end
-
-
 end
-
-
-
 # Driver Code
 fido = Puppy.new
 toy = "rope bone"
 fido.fetch(toy)
-
 fido.sic("Ollie")
 p fido.dog_years(11)
 fido.roll_over
