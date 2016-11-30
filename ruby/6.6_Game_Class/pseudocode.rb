610eed0caa9847aeac38db2502ec91cb51de8791
Pavan Sarguru <pavansarguru@gmail.com>
2016-11-29 22:18:05 -0600

    initial commit

---
+++ ruby/6.6_Game_Class/pseudocode.rb
@@ -0,0 +1,16 @@
+# Function: 
+
+# One user enters a secret word
+# Second user is given a number of dashes equal to the length of the word
+# Prompt tells them they have guesses remaining equal to length of word
+# Prompt tell them they can guess a letter or solve the word
+
+# Get user input
+
+# refer to method that checks if a letter or word
+
+# refer to method that check if the word is correct
+#   display congratulatory method if correct
+#   display conciliatory message if incorrect, show number of remaining guesses
+# refer to method that check if letter is in word
+#   display word with filled in letters, show number of remaining guesses
