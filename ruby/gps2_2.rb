14a6b9aae1148d4e9846ca08eca5264e4760443a
Erica42 <erica.forget@gmail.com>
2016-11-29 20:33:12 -0600

    Add Reflection Section

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -6,7 +6,7 @@
   # call add method
   # set default quantity
   # print the list to the console [can you use one of your other methods here?]
-# output: [what data type goes here, array or hash?]
+# output: [hash]
 g_list = {}
 def create_list(g_list, str)
   g_arr = str.split(" ")
@@ -15,7 +15,6 @@ def create_list(g_list, str)
   end
   g_list
 end
-
 
 # Method to add an item to a list
 # input: item name and optional quantity
@@ -43,7 +42,6 @@ end
 def change_num(g_list, item, num_of)
   add_item_to_list(g_list, item, num_of)
 end
-
 
 # Method to print a list and make it look pretty
 # input: take a list
@@ -79,3 +77,17 @@ prn(next_week_list)
 remove_item(next_week_list, "lemonade")
 change_num(next_week_list, "ice-cream", 1)
 prn(next_week_list)
+
+
+# Reflection Section
+# I learned if you have good pseudocode, especially if you add what you are trying to accomplish within the method, it helps a lot in writing the code. We were halfway done with the project by the time we had all the pseudocode written out.
+# The tradeoffs we had for arrays vs hashes: we liked that you had a key and a value for the item and the quantity of that item, but we were trying to use some array methods on the hash.
+# A method returns the last thing that's being evaluated. And remembering what *is* the last thing being evaluated is tricker than expected sometimes. Like even though we were using .each to do something to a hash, .each returns an array, so we had to make sure to call the hash we had created at the end of the created method.
+# You pass info between methods by either passing it in as a parameter or by calling a method in a different method.
+# Hashes suddenly made a lot more sense after doing this challenge. The fact that changing the value and adding a new item were essentially the same method helped me. Also that delete and setting a default value are both really easy to do.  I don't know why .each is a concept that is hard for me to grasp, but I seem to need to just write a whole bunch of .each loops and to get over my odd dislike of it.
+
+
+
+
+
+
