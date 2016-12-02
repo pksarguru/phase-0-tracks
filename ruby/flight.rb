a83eabbf887899a07943758049c2394457a063a6
Erica42 <erica.forget@gmail.com>
2016-12-01 21:34:42 -0600

    Create module Flight, and class Bird and Plane, and mixedin Flight

---
+++ ruby/flight.rb
@@ -0,0 +1,20 @@
+module Flight
+  def take_off(altitude)
+    puts "Take off and ascending until reaching #{altitude} ..."
+  end
+end
+
+class Bird
+  include Flight
+end
+
+class Plane
+  include Flight
+end
+
+# Driver Code
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
