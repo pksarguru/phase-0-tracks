d29b511b992a676d1dee8662e01f5f7a31b2dc61
Erica42 <erica.forget@gmail.com>
2016-12-02 23:44:48 -0600

    Add some user interface (until loop, printing line, guesses, and requesting a letter) based on the testing

--- ruby/guessing-game/guessing_game.rb
+++ ruby/guessing-game/guessing_game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_reader :guesses, :word, :line, :guessed_arr
+  attr_reader :guesses, :word, :line, :guessed_arr, :line_arr
   def initialize(word)
     @word = word.split("")
     @guesses = word.length * 2
@@ -11,7 +11,6 @@ class Game
   def make_a_guess(letter)
     if @guessed_arr.include?(letter)
       p "You've already guessed that."
-      break
     else
       if @word.include?(letter)
         @line_arr.delete_at(@word.index(letter))
@@ -25,7 +24,15 @@ class Game
     end
   end
 
+end
 
-
-
+# user interface
+game = Game.new("hat")
+until game.guesses == 0 || game.line_arr == game.word
+  p game.line
+  p "You have #{game.guesses} guesses."
+  p "Please guess a letter."
+  letter = gets.chomp.downcase
+  game.make_a_guess(letter)
+  p game.line
 end
