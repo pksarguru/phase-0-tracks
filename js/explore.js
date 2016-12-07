3ce3d99acf1f529dade28fa3e631a1b49a7b959f
Pavan Sarguru <pavansarguru@gmail.com>
2016-12-06 22:29:02 -0600

    wrote initial method, reverses input, but initial undefined output

--- js/explore.js
+++ js/explore.js
@@ -3,14 +3,26 @@
 // 
 // Input a string
 
-var str = "hello"
-// 
+var str = "hello";
+
 // Create function
 //  Create a loop that will run for the length of the inputted string
 //  Copy the last element in the inputed string to a new string
 //     if no built-in method, will have to write one
-//  Delete the last element in the string
 //  Make sure the loop returns new string
 
+function reverse(str){
+  // reversestr = "";
 
+  for (var i=str.length; i>-1; i--) {
+    // reversestr = reversestr + str[i];
+    console.log(str[i])
+  }
+
+  // return reversestr;
+
+}
+
+// console.log(reverse("hello"));
+reverse("hello")
 
