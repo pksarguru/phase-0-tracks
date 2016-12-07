c9323e4160d235e12abb625adfb469a42ebdc918
Pavan Sarguru <pavansarguru@gmail.com>
2016-12-06 22:04:13 -0600

    complete psuedocode

---
+++ js/explore.js
@@ -0,0 +1,16 @@
+// Psuedocode - reverse function
+// Input a string and reverse it
+// 
+// Input a string
+
+var str = "hello"
+// 
+// Create function
+//  Create a loop that will run for the length of the inputted string
+//  Copy the last element in the inputed string to a new string
+//     if no built-in method, will have to write one
+//  Delete the last element in the string
+//  Make sure the loop returns new string
+
+
+
