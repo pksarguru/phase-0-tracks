e16584fec47790d31f85f8449d8263a63a622721
Pavan Sarguru <pavansarguru@gmail.com>
2016-12-08 22:10:05 -0600

    write intitial function to generate array with random string data

--- js/algos.js
+++ js/algos.js
@@ -54,7 +54,40 @@ var match = function(obj1, obj2) {
     }
   }
   return isMatch;
-}     
+}
+
+// Release 2
+// Function: generate an array of a given length with random letters inside
+// Input: length of random array
+// Steps: create variable for the random array
+//        create variable with alphabet
+//        initiate a loop that will run inputted number of times 
+//          create variable with random number between 1 and 10 for length of string
+//          initiate loop that will run until reaching randomly generated length
+//            create string variable
+//            create randomly generated index
+//            add letter with rand index of alphabet string to string variable
+//          push string to random array
+
+var randomInput = function(length){
+
+    randomArray = [];
+
+    alpha = "abcdefghijklmnopqrstuvwxyz";
+
+    for (i=0; i<length; i++) {
+      randLength = Math.floor(Math.random()*10)+1;
+      for (i=0; i<randLength; i++){
+        randString = "";
+        randIndex = Math.floor(Math.random()*26);
+        randString = randString + alpha[randIndex];
+      }
+    randomArray.push(randString);
+    }
+
+    return randomArray;
+
+}
 
 // Driver Code
 
@@ -67,6 +100,10 @@ var obj2 = {song: 'Gold Digger', giraffe: 'Necky', horse: 'Seabiscuit'};
 
 
 console.log(match(obj1,obj2));
+
+console.log(randomInput(3));
+
+console.log(randomInput(2));
 
 
 
