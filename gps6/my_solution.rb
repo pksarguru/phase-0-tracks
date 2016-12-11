460a05627c35c3fbfe8c8a8f4441c66d9b398b91
Pavan Sarguru <pavansarguru@gmail.com>
2016-12-10 18:39:03 -0600

    clean up minor errors

--- gps6/my_solution.rb
+++ gps6/my_solution.rb
@@ -97,9 +97,9 @@ end
 
 # Reflection
 
-1. The parent hash syntax uses strings as keys and the nested hashes use symbols as keys
+# 1. The parent hash syntax uses strings as keys and the nested hashes use symbols as keys
 
-2. 
+# 2. 
 
 
 
