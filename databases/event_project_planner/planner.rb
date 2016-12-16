b2a3dea4214959ac414270ddb5ac3ca4c68a5185
Erica42 <erica.forget@gmail.com>
2016-12-16 10:31:29 -0600

    Initial commit and a bit of pseudocode

---
+++ databases/event_project_planner/planner.rb
@@ -0,0 +1,17 @@
+# Enter type of event
+# Enter type of project for the event
+# Enter due date of project
+# Enter date of event
+# User pull up the application and it checks the date now
+# Pulls up by month
+# What is over due ? (optional)
+# What to do
+# What has been done
+# What events are happening
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# will need Faker::Date, Faker::Boolean, Faker::Name?
+#
