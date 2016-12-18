40bd79448faec0fe0d87146ef76c69d6f080df3a
Erica42 <erica.forget@gmail.com>
2016-12-17 21:02:44 -0600

    Create driver code, still working out kinks on printing out the event data in a string once it is entered

--- databases/event_project_planner/planner.rb
+++ databases/event_project_planner/planner.rb
@@ -24,7 +24,7 @@ db = SQLite3::Database.new("planner.db")
 create_events_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS events(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
+    event_name VARCHAR(255),
     type_of_event VARCHAR(255),
     date_of_event DATETIME,
     has_happened BOOLEAN
@@ -39,7 +39,7 @@ SQL
 create_projects_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS projects(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
+    project_name VARCHAR(255),
     type_of_project VARCHAR(255),
     due_date DATETIME,
     date_to_start DATETIME,
@@ -54,11 +54,11 @@ db.execute(create_events_table_cmd)
 db.execute(create_projects_table_cmd)
 #methods for entering info
 def create_event(db, name, type, date, happened)
-  db.execute("INSERT INTO events (name, type_of_event, date_of_event, has_happened) VALUES (?, ?, ?, ?)", [name, type, date, happened])
+  db.execute("INSERT INTO events (event_name, type_of_event, date_of_event, has_happened) VALUES (?, ?, ?, ?)", [name, type, date, happened])
 end
 
 def create_project(db, name, type, due, start, done, event_id)
-db.execute("INSERT INTO projects (name, type_of_project, due_date, date_to_start, is_done, event_id) VALUES (?, ?, ?, ?, ?, ?)", [name, type, due, start, done, event_id])
+db.execute("INSERT INTO projects (project_name, type_of_project, due_date, date_to_start, is_done, event_id) VALUES (?, ?, ?, ?, ?, ?)", [name, type, due, start, done, event_id])
 end
 
 def mark_done(db, id)
@@ -67,26 +67,25 @@ end
 
 #querrying
 def what_events(db)
-  db.execute("SELECT events.name, events.date_of_event FROM events;")
+  db.execute("SELECT events.event_name, events.date_of_event FROM events;")
 end
 
 def what_projects(db)
-  db.execute("SELECT projects.name, projects.due_date FROM projects;")
+  db.execute("SELECT projects.project_name, projects.due_date FROM projects;")
 end
 
 def what_to_do(db)
-  db.execute("SELECT events.name, events.date_of_event, projects.name FROM projects JOIN events ON projects.event_id = events.id;")
+  db.execute("SELECT events.event_name, events.date_of_event, projects.project_name FROM projects JOIN events ON projects.event_id = events.id;")
 end
 
 def when_to_start(db)
-  db.execute("SELECT events.name, projects.name, projects.date_to_start FROM projects JOIN events ON projects.event_id = events.id WHERE is_done = 'false';")
+  db.execute("SELECT events.event_name, projects.project_name, projects.date_to_start FROM projects JOIN events ON projects.event_id = events.id WHERE is_done = 'false';")
 end
 
+def get_id(db, name_of_event)
+  db.execute("SELECT id FROM events WHERE event_name = ?", [name_of_event])
+end
 
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
-# end
 
 # create_event(db, 'Christmas2016', 'seasonal', '2016-12-25', 'false')
 # create_project(db, 'scarf', 'knitting', '2016-12-25', '2016-10-1', 'false', 1)
@@ -95,8 +94,67 @@ end
 # puts what_projects(db)
 # puts what_to_do(db)
 # puts when_to_start(db)
+# puts get_id(db, 'Christmas2016')
 
-puts ""
+puts "Would you like to create an event or a project? (or done to check what you've already entered)"
+answer = gets.chomp
+while answer != "done"
+  if answer == "event"
+    puts "Great! Please enter the name of your event."
+    name = gets.chomp
+    puts "The name of your event is #{name}."
+    puts "Now enter the type of event. (Birthday/Seasonal/Shower)"
+    type = gets.chomp
+    puts "The type of your event is #{type}."
+    puts "When is this going to happen? (Please use YYYY-MM-DD format)"
+    date = gets.chomp
+    puts "The date of the event is #{date}."
+    happened = "false"
+    create_event(db, name, type, date, happened)
+    puts what_events(db)
+  elsif answer == "project"
+    puts "Great! Please enter the name of your project."
+    name = gets.chomp
+    puts "The name of your project is #{name}."
+    puts "Now enter the type of project. (Knitting/Baking)"
+    type = gets.chomp
+    puts "The type of your project is #{type}."
+    puts "When is this need by? (Please use YYYY-MM-DD format)"
+    due = gets.chomp
+    date = Date.parse(due)
+    puts "The project is due on #{due}."
+    puts "How may days is this project going to take?"
+    days = gets.chomp.to_i
+    start_date = date - days
+    start = start_date.to_s
+    puts "You will need to start your project on #{start}."
+    done = "false"
+    puts "What event is this project for?"
+    name_of_event = gets.chomp
+    event_id = get_id(db, name_of_event)#.to_i Is this already an int? may cause issue if it is.
+    create_project(db, name, type, due, start, done, event_id)
+    puts what_projects(db)
+  else
+    puts "I'm sorry, could you type that again?"
+  end
+  puts "Would you like to create another event or project? (If you are done, type done.)"
+  answer = gets.chomp
+end
+
+print_events = db.execute("SELECT events.event_name, events.type_of_event, events.date_of_event FROM events;")
+print_events.each do |event| #event_name, type_of_event, date_of_event,
+  puts "#{events['event_name']} is a #{events['type_of_event']} type of event on #{events['date_of_event']}."
+end
+
+#project_name, type_of_project, due_date, date_to_start, event_id
+# print_projects = db.execute("SELECT projects.project_name, projects.type_of_project, projects.due_date, projects.date_to_start, events.event_name FROM projects JOIN events ON projects.event_id = events.id")
+# kittens = db.execute("SELECT * FROM kittens")
+# kittens.each do |kitten|
+#  puts "#{kitten['name']} is #{kitten['age']}"
+# end
+
+
+
 
 
 
