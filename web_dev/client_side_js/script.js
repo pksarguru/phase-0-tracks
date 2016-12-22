c03fd1c111785b86d459093f47f4f12375b2894f
Erica42 <erica.forget@gmail.com>
2016-12-21 23:09:36 -0600

    Win all the things by replacing text with a gif

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,3 +1,28 @@
 console.log("The script is running, or so we hope!");
 var text = document.getElementsByTagName('P')[0];
 text.style.border = "solid black";
+
+// function myOtherFunction(){
+//   text.style.border = "solid red";
+// }
+
+function myOtherFunction(){
+  //text.hidden = true;
+  //var  = document.getElementById("header");
+  const img = "<img src = 'celebration.gif'>"
+  text.innerHTML = img;
+
+}
+
+function myFunction() {
+    var list = document.getElementsByTagName("UL")[0];
+    list.getElementsByTagName("LI")[0].innerHTML = "Milk";
+}
+
+var list = document.getElementsByTagName("UL")[0];
+list.addEventListener('click', myFunction);
+
+text.addEventListener('click', myOtherFunction);
+
+
+
