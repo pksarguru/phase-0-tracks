99abeaac8f9d87902a9e2f61a2f43843fd2b4d03
Pavan Sarguru <pavansarguru@gmail.com>
2016-12-21 20:40:42 -0600

    create erb template

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -6,6 +6,15 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
+create_complaints_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS complaints(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    campus VARCHAR(255),
+    complaint VARCHAR(255)
+    )
+SQL
+db.execute(create_complaints_table)
 
 # show students on the home page
 get '/' do
@@ -25,3 +34,9 @@ post '/students' do
 end
 
 # add static resources
+
+# create campus sort page
+get '/campus/:campus' do
+  @students = db.execute("SELECT * FROM students")
+  erb :campus
+end
